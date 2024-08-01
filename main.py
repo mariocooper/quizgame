44af6b6b6d55a1b4fdbe1ff9a2e3e10c0e86d23b
mariocooper <mariocooper00@hotmail.com>
2024-08-01 13:33:24 +0100

    initialised questions list

---
+++ main.py
@@ -0,0 +1,22 @@
+questions = [
+    {
+        "prompt": "What country is NOT in the United Kingdom?",
+        "options": ["A: England", "B: Scotland", "C: Ireland", "D: Wales"],
+        "answer": "C"
+    },
+    {
+        "prompt": "Which of the following foods is a carbohydrate?",
+        "options": ["A: Bread", "B: Beef", "C: Butter", "D: Salmon"],
+        "answer": "A"
+    },
+    {
+        "prompt": "What country is NOT in Europe?",
+        "options": ["A: Italy", "B: Portugal", "C: Albania", "D: Morocco"],
+        "answer": "D"
+    },
+    {
+        "prompt": "Which of the following is NOT a country singer?",
+        "options": ["A: Luke Combs", "B: Victor Leksell", "C: Morgan Wallen", "D: Bailey Zimmerman"],
+        "answer": "B"
+    },
+]
